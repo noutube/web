c40d43ee073d1e6a136f39040554ae046acff3f0
Jake Barnes <me@jakebarn.es>
2019-08-24 16:55:02 +1000

    Update dependencies

--- testem.js
+++ testem.js
@@ -2,18 +2,17 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Chromium'
   ],
   launch_in_dev: [
-    'Chrome'
+    'Chromium'
   ],
   browser_args: {
-    Chrome: {
+    Chromium: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
         '--headless',
-        '--disable-gpu',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
