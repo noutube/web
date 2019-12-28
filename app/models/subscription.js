addf421512356c28243719461715db0512cf849e
Jake Barnes <me@jakebarn.es>
2019-12-28 11:26:00 +1100

    Calculate total duration in less dumb way

--- app/models/subscription.js
+++ app/models/subscription.js
@@ -1,5 +1,5 @@
 import { computed, get } from '@ember/object';
-import { alias, filterBy, map } from '@ember/object/computed';
+import { alias, filterBy, map, sum } from '@ember/object/computed';
 
 import Model, { belongsTo, hasMany } from '@ember-data/model';
 
@@ -27,16 +27,12 @@ export default class SubscriptionModel extends Model {
   @alias('sortableTitle') laterSortableTitle;
 
   @map('newItems.@each.video', (item) => item.video) newVideos;
-  @computed('newVideos.@each.duration')
-  get newTotalDuration() {
-    return this.newVideos.map((video) => get(video, 'duration')).reduce((acc, n) => acc + n, 0);
-  }
+  @map('newVideos.@each.duration', (video) => get(video, 'duration')) newDurations;
+  @sum('newDurations') newTotalDuration;
 
   @map('laterItems.@each.video', (item) => item.video) laterVideos;
-  @computed('laterVideos.@each.duration')
-  get laterTotalDuration() {
-    return this.laterVideos.map((video) => get(video, 'duration')).reduce((acc, n) => acc + n, 0);
-  }
+  @map('laterVideos.@each.duration', (video) => get(video, 'duration')) laterDurations;
+  @sum('laterDurations') laterTotalDuration;
 
   @alias('newItems.length') newItemCount;
   @alias('laterItems.length') laterItemCount;
