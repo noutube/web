dbd863689fa2092adc32717e62dba3575fb323f3
Jake Barnes <me@jakebarn.es>
2021-02-01 14:04:55 +1100

    Add scraped attributes to video

--- app/models/video.js
+++ app/models/video.js
@@ -6,6 +6,10 @@ export default class VideoModel extends Model {
   @attr('string') thumbnail;
   @attr('number') duration;
   @attr('date') publishedAt;
+  @attr('boolean') isLive;
+  @attr('boolean') isLiveContent;
+  @attr('boolean') isUpcoming;
+  @attr('date') scheduledAt;
 
   @belongsTo('channel') channel;
   @hasMany('items') items;
