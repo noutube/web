c3b9b04c36dca3f23ff0e24c23f17e7be4529977
Jake Barnes <me@jakebarn.es>
2021-05-27 19:28:27 +1000

    Replace auto size setting with toggle button

--- app/pods/components/player/component.ts
+++ app/pods/components/player/component.ts
@@ -1,5 +1,7 @@
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 import PlayerService from 'nou2ube/services/player';
 import SettingsService from 'nou2ube/services/settings';
@@ -7,4 +9,11 @@ import SettingsService from 'nou2ube/services/settings';
 export default class PlayerComponent extends Component {
   @service declare player: PlayerService;
   @service declare settings: SettingsService;
+
+  @tracked auto = false;
+
+  @action
+  toggleAuto(): void {
+    this.auto = !this.auto;
+  }
 }
