3e6a7889067696d1d4cacd2b20a62625114eb1a7
Jake Barnes <me@jakebarn.es>
2021-07-17 14:52:59 +1000

    Bump dependencies

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,8 @@
 import { setApplication } from '@ember/test-helpers';
 
 import { start } from 'ember-qunit';
-import { setup } from 'qunit-dom';
 import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
 import Application from 'nou2ube/app';
 import config from 'nou2ube/config/environment';
