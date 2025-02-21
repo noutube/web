eb3f58b9716a9b57e43a1aae31d865064bb4d643
Jake Barnes <me@jakebarn.es>
2025-02-21 15:23:57 +1100

    Fix cache header

--- config/deploy.js
+++ config/deploy.js
@@ -1,9 +1,24 @@
 module.exports = () => ({
-  s3: {
+  pipeline: {
+    alias: {
+      s3: { as: ['s3-assets', 's3-other'] }
+    }
+  },
+  's3-assets': {
     bucket: 'noutu.be',
     region: 'us-east-1',
     acl: false,
-    filePattern: '**/*'
+    filePattern: 'assets/**/*',
+    expires: null
+  },
+  's3-other': {
+    bucket: 'noutu.be',
+    region: 'us-east-1',
+    acl: false,
+    filePattern: '**/*',
+    fileIgnorePattern: 'assets/**/*',
+    cacheControl: 'max-age=0, no-cache',
+    expires: null
   },
   cloudfront: {
     distribution: process.env.AWS_DISTRIBUTION
