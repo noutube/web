44269241c07af450c7b03262af30ac4d2029c183
Jake Barnes <me@jakebarn.es>
2025-02-25 16:52:30 +1100

    Check and show warning when revision outdated

--- app/pods/application/route.ts
+++ app/pods/application/route.ts
@@ -2,15 +2,18 @@ import Transition from '@ember/routing/-private/transition';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
+import RevisionService from 'noutube/services/revision';
 import SessionService from 'noutube/services/session';
 import SettingsService from 'noutube/services/settings';
 
 export default class ApplicationRoute extends Route {
+  @service declare revision: RevisionService;
   @service declare session: SessionService;
   @service declare settings: SettingsService;
 
   async beforeModel(transition: Transition): Promise<void> {
     this.settings.applyTheme();
     await this.session.restore();
+    this.revision.start();
   }
 }
